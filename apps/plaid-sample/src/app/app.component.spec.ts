3c25ffc435e00ebc5a440c9e19ada51ba713d5b5
David Matheson <davidfmatheson@gmail.com>
2023-12-25 16:46:08 -0500

    Make test names more neotest-jest-friendly.

--- apps/plaid-sample/src/app/app.component.spec.ts
+++ apps/plaid-sample/src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
     );
   });
 
-  it(`should have as title 'plaid-sample'`, () => {
+  it('should have as title plaid-sample', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('plaid-sample');
