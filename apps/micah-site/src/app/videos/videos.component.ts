10147a5378af3db1aac953df2ce0e7f7f90044ad
David Matheson <davidfmatheson@gmail.com>
2024-05-26 18:58:07 -0400

    Get carousel working for videos and pictures

--- apps/micah-site/src/app/videos/videos.component.ts
+++ apps/micah-site/src/app/videos/videos.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import {
+  CarouselComponent,
+  CarouselControlComponent,
+  CarouselIndicatorsComponent,
+  CarouselInnerComponent,
+  CarouselItemComponent,
+  ThemeDirective,
+} from '@coreui/angular';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+export type Video = {
+ src: string;
+ url?: SafeResourceUrl;
+}
 
 @Component({
   selector: 'app-videos',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ThemeDirective,
+    CarouselComponent,
+    CarouselIndicatorsComponent,
+    CarouselInnerComponent,
+    CarouselItemComponent,
+    CarouselControlComponent,
+    RouterModule,
+  ],
   templateUrl: './videos.component.html',
   styleUrl: './videos.component.scss',
 })
-export class VideosComponent {}
+export class VideosComponent {
+  sanitizer: DomSanitizer;
+
+  videos = signal<Video[]>([
+    {
+      src: "https://www.youtube.com/embed/FwAiXk_aBI8?si=H6mfJajTeI5xbBh7"
+    }
+  ]);
+
+  constructor(sanitizer: DomSanitizer) {
+    this.sanitizer = sanitizer;
+    this.videos.update(values => {
+      values.forEach(value => value.url = this.sanitizer.bypassSecurityTrustResourceUrl(value.src))
+      return values;
+    })
+  }
+}
