0dcbc5a20874c7d1909e99b64574f07d7c015072
David Matheson <davidfmatheson@gmail.com>
2024-05-28 19:25:14 -0400

    Needed to fix the test

--- apps/micah-site/src/app/videos/videos.component.spec.ts
+++ apps/micah-site/src/app/videos/videos.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { VideosComponent } from './videos.component';
+import { RouterModule } from '@angular/router';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('VideosComponent', () => {
   let component: VideosComponent;
   let fixture: ComponentFixture<VideosComponent>;
+  let intersectionObserverMock: jest.Func;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [VideosComponent],
+      imports: [VideosComponent, NoopAnimationsModule, RouterModule.forRoot([])],
     }).compileComponents();
+
+    intersectionObserverMock = () => ({
+      observe: () => null,
+      unobserve: () => null
+    });
+    window.IntersectionObserver = jest.fn().mockImplementation(intersectionObserverMock);
 
     fixture = TestBed.createComponent(VideosComponent);
     component = fixture.componentInstance;
