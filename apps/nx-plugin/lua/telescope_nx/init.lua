93ae62a9781c70df4155fc27233dcd49ec617018
David Matheson <davidfmatheson@gmail.com>
2024-07-06 18:29:11 -0400

    Building up a list of nx projects

--- apps/nx-plugin/lua/telescope_nx/init.lua
+++ apps/nx-plugin/lua/telescope_nx/init.lua
@@ -1,17 +1,35 @@
 local pickers = require('telescope.pickers')
 local finders = require('telescope.finders')
+local previewers = require('telescope.previewers')
+local utils = require('telescope.previewers.utils')
 local config = require('telescope.config').values
+
+local log = require('plenary.log'):new()
+log.level = 'debug'
 
 local M = {}
 
 M.show_nx_console = function (opts)
   pickers.new(opts, {
-    finder = finders.new_table({
-      "Yes",
-      "No",
-      "Maybe",
+    finder = finders.new_async_job({
+      command_generator = function ()
+        return { "npx", "nx", "show", "projects" }
+      end,
+      entry_maker = function (entry)
+        return {
+          value = entry,
+          display = entry,
+          ordinal = entry,
+        }
+      end
     }),
     sorter = config.generic_sorter(opts),
+    previewer = previewers.new_buffer_previewer({
+      title = "Nx Available Commands",
+      define_preview = function (self, entry)
+        vim.api.nvim_buf_set_lines(self.state.bufnr, 0, 0, true, vim.split(vim.inspect(entry.value), "\n"))
+      end,
+    }),
   }):find()
 end
 
