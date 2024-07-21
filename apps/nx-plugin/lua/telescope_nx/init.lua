dd3ae060d92a70943e72872b961327e45048ced4
David Matheson <davidfmatheson@gmail.com>
2024-07-21 11:57:14 -0400

    Nx plugin somewhat working

--- apps/nx-plugin/lua/telescope_nx/init.lua
+++ apps/nx-plugin/lua/telescope_nx/init.lua
@@ -9,25 +9,37 @@ log.level = 'debug'
 
 local M = {}
 
+local shell_script = [[
+  for project in `npx nx show projects`; do
+    npx nx show project $project --json
+  done
+]]
+
 M.show_nx_console = function (opts)
+  opts = opts or {}
   pickers.new(opts, {
     finder = finders.new_async_job({
       command_generator = function ()
-        return { "npx", "nx", "show", "projects" }
+        return { "sh", "-c", shell_script }
       end,
       entry_maker = function (entry)
-        return {
-          value = entry,
-          display = entry,
-          ordinal = entry,
-        }
+        local parsed = vim.json.decode(entry)
+        if parsed then
+          return {
+            value = parsed,
+            display = parsed.name,
+            ordinal = parsed.name,
+          }
+        end
       end
     }),
     sorter = config.generic_sorter(opts),
     previewer = previewers.new_buffer_previewer({
       title = "Nx Available Commands",
       define_preview = function (self, entry)
-        vim.api.nvim_buf_set_lines(self.state.bufnr, 0, 0, true, vim.split(vim.inspect(entry.value), "\n"))
+        local targets = {}
+        for k,v in pairs(entry.value.targets) do table.insert(targets, k) end
+        vim.api.nvim_buf_set_lines(self.state.bufnr, 0, 0, true, vim.split(vim.inspect(targets), ","))
       end,
     }),
   }):find()
