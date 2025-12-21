5b890053d4ccbf9b6c95146e4a622294be62c8e2
David Matheson <davidfmatheson@gmail.com>
2025-12-21 10:26:52 -0500

    Project renaming in config

--- apps/plaid-sample-api/jest.config.ts
+++ apps/plaid-sample-api/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'plain-sample-api',
+  displayName: 'plaid-sample-api',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
